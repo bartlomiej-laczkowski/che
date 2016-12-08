4a2bfe62b032ec33824930fc3a38716a01cceb5a
Igor Vinokur <ivinokur@codenvy.com>
2016-12-08 09:49:44 +0200

    commit

--- plugins/plugin-github/che-plugin-github-ide/src/main/java/org/eclipse/che/plugin/github/ide/importer/page/GithubImporterPagePresenter.java
+++ plugins/plugin-github/che-plugin-github-ide/src/main/java/org/eclipse/che/plugin/github/ide/importer/page/GithubImporterPagePresenter.java
@@ -53,17 +53,16 @@ import java.util.Map;
  */
 public class GithubImporterPagePresenter extends AbstractWizardPage<MutableProjectConfig> implements GithubImporterPageView.ActionDelegate {
 
-    private static final String HOST_PATTERN = "([A-Za-z0-9]+(\\.|\\-)?)*[A-Za-z0-9]+";
-    private static final String REPO_PATTERN = "([A-Za-z0-9]+(\\.|\\-|/)?)*[A-Za-z0-9]+/?$";
-
     // An alternative scp-like syntax: [user@]host.xz:path/to/repo.git/
-    private static final RegExp SCP_LIKE_SYNTAX = RegExp.compile("^([A-Za-z0-9_\\.\\-]+@)?" + HOST_PATTERN + ":" + REPO_PATTERN);
+    private static final RegExp SCP_LIKE_SYNTAX = RegExp.compile("([A-Za-z0-9_\\-]+\\.[A-Za-z0-9_\\-:]+)+:");
     // the transport protocol
-    private static final RegExp PROTOCOL        = RegExp.compile("(http|https|git|ssh|ftp|ftps)://");
-    // the address of the remote server between // or @ and : or /
-    private static final RegExp HOST            = RegExp.compile("(//|@)" + HOST_PATTERN + "(/|:)");
-    // the repository path or name
-    private static final RegExp REPO            = RegExp.compile("/" + REPO_PATTERN);
+    private static final RegExp PROTOCOL        = RegExp.compile("((http|https|git|ssh|ftp|ftps)://)");
+    // the address of the remote server between // and /
+    private static final RegExp HOST1           = RegExp.compile("//([A-Za-z0-9_\\-]+\\.[A-Za-z0-9_\\-:]+)+/");
+    // the address of the remote server between @ and : or /
+    private static final RegExp HOST2           = RegExp.compile("@([A-Za-z0-9_\\-]+\\.[A-Za-z0-9_\\-:]+)+[:/]");
+    // the repository name
+    private static final RegExp REPO_NAME       = RegExp.compile("/[A-Za-z0-9_.\\-]+$");
     // start with white space
     private static final RegExp WHITE_SPACE     = RegExp.compile("^\\s");
 
@@ -467,13 +466,13 @@ public class GithubImporterPagePresenter extends AbstractWizardPage<MutableProje
             return false;
         }
 
-        if (!HOST.test(url)) {
+        if (!(HOST1.test(url) || HOST2.test(url))) {
             view.markURLInvalid();
             view.setURLErrorMessage(locale.importProjectMessageHostIncorrect());
             return false;
         }
 
-        if (!REPO.test(url)) {
+        if (!(REPO_NAME.test(url))) {
             view.markURLInvalid();
             view.setURLErrorMessage(locale.importProjectMessageNameRepoIncorrect());
             return false;
